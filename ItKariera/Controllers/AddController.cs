36b69603f74476b526d678922efd3e2e595a658b
TonyHub08 <tony.development008@gmail.com>
2026-03-27 19:12:25 +0200

    Upload my files
    Upload my file, I made some of the interface, made a nav bar with some routes and views + connected the database

---
+++ ItKariera/Controllers/AddController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItKariera.Controllers
+{
+    public class AddController : Controller
+    {
+        public IActionResult Add()
+        {
+            return View("Add");
+        }
+    }
+}
---
+++ ItKariera/Controllers/ProfileController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ItKariera.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View("Profile");
+        }
+    }
+}
